88c9a0306d5b45976dd37d4663cdc8af29a3fa85
Techbrolakes <lekandar11@gmail.com>
2023-05-12 16:46:07 +0100

    Feat: Product && Category API V4

--- source/controllers/product.controller.ts
+++ source/controllers/product.controller.ts
@@ -7,42 +7,189 @@ import productService from '../services/product.service';
 import { Types } from 'mongoose';
 
 /****
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
  *
- *
- *  Update Category
+ * PRODUCT RELATED CONTROLLERS
+ * * * * * * * * * * * * *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
  */
 
-export const updateProductCategory = async (req: ExpressRequest, res: Response): Promise<Response | void> => {
+// Create Product Controller For Admin
+export const createProduct = async (req: ExpressRequest, res: Response): Promise<Response | void> => {
    try {
-      const { category_description, category_image, category_name } = req.body;
+      const { category_id, product_name, product_price, product_description, product_image, product_category, product_quantity } = req.body;
 
-      const category_id = new Types.ObjectId(req.params.category_id);
-
-      const category = await productService.getProductCategoryById({ category_id });
-
-      if (!category) {
+      // Check if category_id is provided
+      if (!category_id) {
          return ResponseHandler.sendErrorResponse({
+            res,
+            code: HTTP_CODES.BAD_REQUEST,
+            error: 'Category ID is required',
+         });
+      }
+
+      // Check if category exist
+      const existingCategory = await productService.getProductCategoryById({ category_id: new Types.ObjectId(category_id) });
+
+      // If category does not exist
+      if (!existingCategory) {
+         return ResponseHandler.sendErrorResponse({
+            res,
             code: HTTP_CODES.NOT_FOUND,
             error: 'Category does not exist',
+         });
+      }
+
+      // Check if product name is provided
+      const existingProductName = await productService.getByProductName({ product_name: product_name.toLowerCase() });
+
+      // If product name exist
+      if (existingProductName) {
+         return ResponseHandler.sendErrorResponse({ res, code: HTTP_CODES.BAD_REQUEST, error: `${product_name} already exist in the database` });
+      }
+
+      // Create product
+      const product = await productService.createProduct({
+         category_id,
+         product_name: product_name.toLowerCase(),
+         product_price,
+         product_description,
+         product_image,
+         product_category,
+         product_quantity,
+      });
+
+      // Return response
+      return ResponseHandler.sendSuccessResponse({
+         res,
+         code: HTTP_CODES.CREATED,
+         message: `${product_name} added to the database`,
+         data: product,
+      });
+   } catch (error) {
+      // Return error response
+      ResponseHandler.sendErrorResponse({
+         code: HTTP_CODES.INTERNAL_SERVER_ERROR,
+         error: `${error}`,
+         res,
+      });
+   }
+};
+
+// Get Product By Category
+export const getProductByCategory = async (req: ExpressRequest, res: Response): Promise<Response | void> => {
+   try {
+      // Get category id
+      const category_id = req.params.category_id;
+
+      // Check if category id is provided
+      const category = await productService.getProductByCategoryId({ category_id: new Types.ObjectId(category_id) });
+
+      // If category does not exist
+
+      if (category) {
+         // Return response
+
+         return ResponseHandler.sendSuccessResponse({
+            res,
+            code: HTTP_CODES.OK,
+            message: 'Product category retrieved successfully',
+            data: category,
+         });
+      }
+   } catch (error) {
+      // Return error response
+
+      return ResponseHandler.sendErrorResponse({
+         code: HTTP_CODES.INTERNAL_SERVER_ERROR,
+         error: `${error}`,
+         res,
+      });
+   }
+};
+
+// Update Product
+export const updateProduct = async (req: ExpressRequest, res: Response): Promise<Response | void> => {
+   try {
+      const { category_id, product_name, product_price, product_description, product_image, product_category, product_quantity } = req.body;
+
+      // Check if product id is provided
+      const product_id = new Types.ObjectId(req.params.product_id);
+
+      // Check if product exist
+      const product = await productService.getProductById({ product_id });
+
+      // If product does not exist
+      if (!product) {
+         return ResponseHandler.sendErrorResponse({
+            code: HTTP_CODES.NOT_FOUND,
+            error: 'Product does not exist',
             res,
          });
       }
 
-      const updatedCategory = await productService.atomicCategoryUpdate(category_id, {
+      // Update product
+      const updatedProduct = await productService.atomicUpdate(product_id, {
          $set: {
-            category_description,
-            category_image,
-            category_name,
+            product_name,
+            product_price,
+            product_description,
+            product_image,
+            product_category,
+            product_quantity,
+            category_id: new Types.ObjectId(category_id),
          },
       });
 
+      // Return response
       return ResponseHandler.sendSuccessResponse({
          res,
          code: HTTP_CODES.OK,
-         message: 'Category updated successfully',
-         data: updatedCategory,
+         message: 'Product updated successfully',
+         data: updatedProduct,
       });
    } catch (error) {
+      // Return error response
+      return ResponseHandler.sendErrorResponse({
+         code: HTTP_CODES.INTERNAL_SERVER_ERROR,
+         error: `${error}`,
+         res,
+      });
+   }
+};
+
+// Delete Product
+export const deleteProduct = async (req: ExpressRequest, res: Response): Promise<Response | void> => {
+   try {
+      // Check if product id is provided
+      const product_id = new Types.ObjectId(req.params.product_id);
+
+      // Check if product exist
+      const product = await productService.getProductById({ product_id });
+
+      // If product does not exist
+      if (!product) {
+         return ResponseHandler.sendErrorResponse({
+            code: HTTP_CODES.NOT_FOUND,
+            error: 'Product does not exist',
+            res,
+         });
+      }
+
+      // Delete product
+      await productService.deleteProduct(product_id);
+
+      // Return response
+      return ResponseHandler.sendSuccessResponse({
+         res,
+         code: HTTP_CODES.OK,
+         message: 'Product deleted successfully',
+      });
+   } catch (error) {
+      // Return error response
       return ResponseHandler.sendErrorResponse({
          code: HTTP_CODES.INTERNAL_SERVER_ERROR,
          error: `${error}`,
@@ -52,11 +199,125 @@ export const updateProductCategory = async (req: ExpressRequest, res: Response):
 };
 
 /****
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
  *
- *
- *  Delete Category
+ * PRODUCT CATEGORY RELATED CONTROLLERS
+ * * * * * * * * * * * * *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
  */
 
+// Create Product Category
+export const createProductCategory = async (req: ExpressRequest, res: Response): Promise<Response | void> => {
+   try {
+      const { category_description, category_image, category_name } = req.body;
+
+      // Gets the category name
+      const existingCategoryName = await productService.getByCategoryName({ category_name: category_name.toLowerCase() });
+
+      // If category name exists
+      if (existingCategoryName) {
+         return ResponseHandler.sendErrorResponse({ res, code: HTTP_CODES.BAD_REQUEST, error: `${category_name} category already exist` });
+      }
+
+      // Create category
+      const category = await productService.createProductCategory({
+         category_description,
+         category_image,
+         category_name: category_name.toLowerCase(),
+      });
+
+      // Return response
+      return ResponseHandler.sendSuccessResponse({
+         res,
+         code: HTTP_CODES.CREATED,
+         message: `${category_name} Category added to the database`,
+         data: category,
+      });
+   } catch (error) {
+      // Return error response
+      ResponseHandler.sendErrorResponse({
+         code: HTTP_CODES.INTERNAL_SERVER_ERROR,
+         error: `${error}`,
+         res,
+      });
+   }
+};
+
+// Get Product Categories
+export const getProductCategories = async (req: ExpressRequest, res: Response): Promise<Response | void> => {
+   try {
+      // Get product categories
+      const categories = await productService.getProductCategories();
+
+      // Return response
+      if (categories) {
+         return ResponseHandler.sendSuccessResponse({
+            res,
+            code: HTTP_CODES.OK,
+            message: 'Product categories retrieved successfully',
+            data: categories,
+         });
+      }
+   } catch (error) {
+      // Return error response
+      return ResponseHandler.sendErrorResponse({
+         code: HTTP_CODES.INTERNAL_SERVER_ERROR,
+         error: `${error}`,
+         res,
+      });
+   }
+};
+
+// Update Product Category
+export const updateProductCategory = async (req: ExpressRequest, res: Response): Promise<Response | void> => {
+   try {
+      const { category_description, category_image, category_name } = req.body;
+
+      // Check if category id is provided
+      const category_id = new Types.ObjectId(req.params.category_id);
+
+      // Check if category exist
+      const category = await productService.getProductCategoryById({ category_id });
+
+      // If category does not exist
+      if (!category) {
+         return ResponseHandler.sendErrorResponse({
+            code: HTTP_CODES.NOT_FOUND,
+            error: 'Category does not exist',
+            res,
+         });
+      }
+
+      // Update category
+      const updatedCategory = await productService.atomicCategoryUpdate(category_id, {
+         $set: {
+            category_description,
+            category_image,
+            category_name,
+         },
+      });
+
+      // Return response
+      return ResponseHandler.sendSuccessResponse({
+         res,
+         code: HTTP_CODES.OK,
+         message: 'Category updated successfully',
+         data: updatedCategory,
+      });
+   } catch (error) {
+      // Return error response
+      return ResponseHandler.sendErrorResponse({
+         code: HTTP_CODES.INTERNAL_SERVER_ERROR,
+         error: `${error}`,
+         res,
+      });
+   }
+};
+
+// Delete Product Category
 export const deleteProductCategory = async (req: ExpressRequest, res: Response): Promise<Response | void> => {
    try {
       const category_id = new Types.ObjectId(req.params.category_id);
@@ -95,242 +356,6 @@ export const deleteProductCategory = async (req: ExpressRequest, res: Response):
       });
    } catch (error) {
       return ResponseHandler.sendErrorResponse({
-         code: HTTP_CODES.INTERNAL_SERVER_ERROR,
-         error: `${error}`,
-         res,
-      });
-   }
-};
-
-/****
- *
- *
- *  Delete Product
- */
-
-export const deleteProduct = async (req: ExpressRequest, res: Response): Promise<Response | void> => {
-   try {
-      const product_id = new Types.ObjectId(req.params.product_id);
-
-      const product = await productService.getProductById({ product_id });
-
-      if (!product) {
-         return ResponseHandler.sendErrorResponse({
-            code: HTTP_CODES.NOT_FOUND,
-            error: 'Product does not exist',
-            res,
-         });
-      }
-
-      await productService.deleteProduct(product_id);
-
-      return ResponseHandler.sendSuccessResponse({
-         res,
-         code: HTTP_CODES.OK,
-         message: 'Product deleted successfully',
-      });
-   } catch (error) {
-      return ResponseHandler.sendErrorResponse({
-         code: HTTP_CODES.INTERNAL_SERVER_ERROR,
-         error: `${error}`,
-         res,
-      });
-   }
-};
-
-/****
- *
- *
- *  Edit Product
- */
-
-export const updateProduct = async (req: ExpressRequest, res: Response): Promise<Response | void> => {
-   try {
-      const { category_id, product_name, product_price, product_description, product_image, product_category, product_quantity } = req.body;
-
-      const product_id = new Types.ObjectId(req.params.product_id);
-
-      const product = await productService.getProductById({ product_id });
-
-      if (!product) {
-         return ResponseHandler.sendErrorResponse({
-            code: HTTP_CODES.NOT_FOUND,
-            error: 'Product does not exist',
-            res,
-         });
-      }
-
-      const updatedProduct = await productService.atomicUpdate(product_id, {
-         $set: {
-            product_name,
-            product_price,
-            product_description,
-            product_image,
-            product_category,
-            product_quantity,
-            category_id: new Types.ObjectId(category_id),
-         },
-      });
-
-      return ResponseHandler.sendSuccessResponse({
-         res,
-         code: HTTP_CODES.OK,
-         message: 'Product updated successfully',
-         data: updatedProduct,
-      });
-   } catch (error) {
-      return ResponseHandler.sendErrorResponse({
-         code: HTTP_CODES.INTERNAL_SERVER_ERROR,
-         error: `${error}`,
-         res,
-      });
-   }
-};
-
-/****
- *
- *
- *  Get Product By Category
- */
-export const getProductByCategory = async (req: ExpressRequest, res: Response): Promise<Response | void> => {
-   try {
-      const category_id = req.params.category_id;
-      const category = await productService.getProductByCategoryId({ category_id: new Types.ObjectId(category_id) });
-
-      console.log(category);
-
-      if (category) {
-         return ResponseHandler.sendSuccessResponse({
-            res,
-            code: HTTP_CODES.OK,
-            message: 'Product category retrieved successfully',
-            data: category,
-         });
-      }
-   } catch (error) {
-      return ResponseHandler.sendErrorResponse({
-         code: HTTP_CODES.INTERNAL_SERVER_ERROR,
-         error: `${error}`,
-         res,
-      });
-   }
-};
-
-/****
- *
- *
- * Admin Get Product Category
- */
-
-export const getProductCategories = async (req: ExpressRequest, res: Response): Promise<Response | void> => {
-   try {
-      const categories = await productService.getProductCategories();
-      if (categories) {
-         return ResponseHandler.sendSuccessResponse({
-            res,
-            code: HTTP_CODES.OK,
-            message: 'Product categories retrieved successfully',
-            data: categories,
-         });
-      }
-   } catch (error) {
-      return ResponseHandler.sendErrorResponse({
-         code: HTTP_CODES.INTERNAL_SERVER_ERROR,
-         error: `${error}`,
-         res,
-      });
-   }
-};
-
-/****
- *
- *
- * Admin Create Product Category
- */
-
-export const createProductCategory = async (req: ExpressRequest, res: Response): Promise<Response | void> => {
-   try {
-      const { category_description, category_image, category_name } = req.body;
-
-      const existingCategoryName = await productService.getByCategoryName({ category_name: category_name.toLowerCase() });
-
-      if (existingCategoryName) {
-         return ResponseHandler.sendErrorResponse({ res, code: HTTP_CODES.BAD_REQUEST, error: `${category_name} category already exist` });
-      }
-
-      const category = await productService.createProductCategory({
-         category_description,
-         category_image,
-         category_name: category_name.toLowerCase(),
-      });
-
-      return ResponseHandler.sendSuccessResponse({
-         res,
-         code: HTTP_CODES.CREATED,
-         message: `${category_name} Category added to the database`,
-         data: category,
-      });
-   } catch (error) {
-      ResponseHandler.sendErrorResponse({
-         code: HTTP_CODES.INTERNAL_SERVER_ERROR,
-         error: `${error}`,
-         res,
-      });
-   }
-};
-
-/****
- *
- *
- * Admin Create Product
- */
-
-export const createProduct = async (req: ExpressRequest, res: Response): Promise<Response | void> => {
-   try {
-      const { category_id, product_name, product_price, product_description, product_image, product_category, product_quantity } = req.body;
-
-      if (!category_id) {
-         return ResponseHandler.sendErrorResponse({
-            res,
-            code: HTTP_CODES.BAD_REQUEST,
-            error: 'Category ID is required',
-         });
-      }
-
-      const existingCategory = await productService.getProductCategoryById({ category_id: new Types.ObjectId(category_id) });
-
-      if (!existingCategory) {
-         return ResponseHandler.sendErrorResponse({
-            res,
-            code: HTTP_CODES.NOT_FOUND,
-            error: 'Category does not exist',
-         });
-      }
-
-      const existingProductName = await productService.getByProductName({ product_name: product_name.toLowerCase() });
-
-      if (existingProductName) {
-         return ResponseHandler.sendErrorResponse({ res, code: HTTP_CODES.BAD_REQUEST, error: `${product_name} already exist in the database` });
-      }
-
-      const product = await productService.createProduct({
-         category_id,
-         product_name: product_name.toLowerCase(),
-         product_price,
-         product_description,
-         product_image,
-         product_category,
-         product_quantity,
-      });
-
-      return ResponseHandler.sendSuccessResponse({
-         res,
-         code: HTTP_CODES.CREATED,
-         message: `${product_name} added to the database`,
-         data: product,
-      });
-   } catch (error) {
-      ResponseHandler.sendErrorResponse({
          code: HTTP_CODES.INTERNAL_SERVER_ERROR,
          error: `${error}`,
          res,
